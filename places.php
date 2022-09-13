5f0e5c8df9f4d603ad92d6adf97b3a0c59a16010
tomas <tmaldonadotrs@gmail.com>
2022-09-13 00:42:07 -0700

    Improved column layout. Image section now working as desired. In the process of changing SERP APIs due to usage limit on current api. Working on creating original api to use w/o limits. WIll incorporate shortly.

--- places.php
+++ places.php
@@ -132,28 +132,17 @@ echo 'No results to display.';
     <h3>Average Weather</h3>
     <h3>Did You Know?</h3>
     <h3>Some Facts About This Place</h3>
-</div>
 
-</section>
+    <?php
 
-<section id="already-been">
-    <div id="column-ab-1">
-    <!-- ajax see more section-->
-    
-
-
-
-
-<?php
-/*
-$ch = curl_init();
+/*$ch = curl_init();
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HEADER, false);
 
+
 $data = [
-   "q" => $country, $city, $landmark,
-// "q" => 'venice',
+   "q" => $country, $city, $landmark,  
    "tbm" => "isch",
    "num" => "5",
 ];
@@ -171,16 +160,65 @@ $json = json_decode($response, true);
 
 // var_dump($json);
 
+*/
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+	CURLOPT_URL => "https://duckduckgo-duckduckgo-zero-click-info.p.rapidapi.com/?q=?$country,$city,$landmark&format=json&skip_disambig=1&no_redirect=1&no_html=1&callback=process_duckduckgo",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+		"X-RapidAPI-Host: duckduckgo-duckduckgo-zero-click-info.p.rapidapi.com",
+		"X-RapidAPI-Key: 39895f3948mshc954d1855356b54p15bcacjsn1c6cdc2ab2cd"
+	],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+$json = json_decode($response);
+
+var_dump($json);
+
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	echo $response;
+}
+
+
 $image_results = $json['image_results'];
- 
+
+    echo '<div id=container-figure>'; 
 foreach($image_results as $result) {
     $source = $result['sourceUrl'];
  
-    echo '<figure>
+
+    echo '<figure class=figure-img>
     <img class=img src="'.$source.'">
     </figure>';
-}*/
+    '</div>';
+}
 ?>
+
+</div>
+
+</section>
+
+<button id="already-btn">+</button>
+<section id="already-been">
+    <div id="column-ab-1">
+    <!-- ajax see more section-->
+    
     </div >
     </section>
 
